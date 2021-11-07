e51d895c584851a203b65cbb5a0d61a9285076f5
yuto.eguma <yuto.eguma@gmail.com>
2021-11-08 00:09:16 +0900

    エンドポイントの追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,23 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+admin.initializeApp();
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+export const addMessage = functions.https.onRequest(async (req, res) => {
+    const original = req.query.text;
+    const writeResult = await admin.firestore().collection('message').add({original: original});
+    res.json({result: `Message with ID; ${writeResult.id} added.`})
+})
+
+export const makeUppercase = functions.firestore.document('/messages/{documentId}')
+    .onCreate((snap, context) => {
+        const original = snap.data().original;
+        functions.logger.log('Uppercasing', context.params.documentId, original);
+        const uppercase = original.toUpperCase();
+        return snap.ref.set({uppercase}, {merge: true})
+    })
