34773a7bbdec851cbdd3d79f36c7c217f67183d4
yuto.eguma <yuto.eguma@gmail.com>
2021-11-08 00:20:15 +0900

    lint:fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,15 +9,17 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 });
 
 export const addMessage = functions.https.onRequest(async (req, res) => {
-    const original = req.query.text;
-    const writeResult = await admin.firestore().collection('message').add({original: original});
-    res.json({result: `Message with ID; ${writeResult.id} added.`})
-})
+  const original = req.query.text;
+  const writeResult = await admin.firestore()
+      .collection("message").add({original: original});
+  res.json({result: `Message with ID; ${writeResult.id} added.`});
+});
 
-export const makeUppercase = functions.firestore.document('/messages/{documentId}')
+export const makeUppercase = functions.firestore
+    .document("/messages/{documentId}")
     .onCreate((snap, context) => {
-        const original = snap.data().original;
-        functions.logger.log('Uppercasing', context.params.documentId, original);
-        const uppercase = original.toUpperCase();
-        return snap.ref.set({uppercase}, {merge: true})
-    })
+      const original = snap.data().original;
+      functions.logger.log("Uppercasing", context.params.documentId, original);
+      const uppercase = original.toUpperCase();
+      return snap.ref.set({uppercase}, {merge: true});
+    });
