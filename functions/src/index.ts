62e4e7e2e1a679fd729fc7c14f74a9ae47bbdd88
yuto.eguma <yuto.eguma@gmail.com>
2021-11-09 00:09:14 +0900

    レスポンスを json に

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,7 @@ admin.initializeApp();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.json({message: "Hello from Firebase!"});
 });
 
 export const addMessage = functions.https.onRequest(async (req, res) => {
