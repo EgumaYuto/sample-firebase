bf8da806e61b5499ee86f4a673bd1b8dadadfa36
yuto.eguma <yuto.eguma@gmail.com>
2021-11-21 18:52:36 +0900

    ログイン画面の追加

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
 
 const Login: NextPage = () => {
     return (
-        <div>
-            Login
+        <div className={styles.container}>
+            <form>
+                <label>
+                    email
+                    <input type="text"></input>
+                </label>
+                <label>
+                    password
+                    <input type="password"></input>
+                </label>
+                <input type="submit"></input>
+            </form>
         </div>
     )
 }
