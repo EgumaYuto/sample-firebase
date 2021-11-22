d934c7653dc93a28075ac02e6e03aae4b41910cf
yuto.eguma <yuto.eguma@gmail.com>
2021-11-22 20:33:44 +0900

    トリガーを切り出し

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,23 +1,16 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import * as message from "./http/message";
+import * as httpMessage from "./http/message";
+import * as triggerMessage from "./trigger/message";
 
 admin.initializeApp();
 
 export {
-  message,
+  httpMessage,
+  triggerMessage,
 };
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.json({message: "Hello from Firebase!"});
 });
-
-export const makeUppercase = functions.firestore
-    .document("/message/{documentId}")
-    .onCreate((snap, context) => {
-      const original = snap.data().original;
-      functions.logger.log("Uppercasing", context.params.documentId, original);
-      const uppercase = original.toUpperCase();
-      return snap.ref.set({uppercase}, {merge: true});
-    });
---
+++ functions/src/trigger/message.ts
@@ -0,0 +1,10 @@
+import * as functions from "firebase-functions";
+
+export const makeUppercase = functions.firestore
+    .document("/message/{documentId}")
+    .onCreate((snap, context) => {
+      const original = snap.data().original;
+      functions.logger.log("Uppercasing", context.params.documentId, original);
+      const uppercase = original.toUpperCase();
+      return snap.ref.set({uppercase}, {merge: true});
+    });
