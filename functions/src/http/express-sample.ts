34ec7f91eec8d5d97276172abeba633b4dfb9ee4
yuto.eguma <yuto.eguma@gmail.com>
2021-11-25 19:43:49 +0900

    Revert "datadog agent の追加"
    This reverts commit 8fbefd3e232b0a0b969c84211351ae47ce18af36.

--- functions/src/http/express-sample.ts
+++ functions/src/http/express-sample.ts
@@ -1,6 +1,5 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
-import * as datadog from "connect-datadog"
 
 const router = express.Router();
 router.get("/", (_req, res) => {
@@ -13,10 +12,6 @@ router.get("/path1", (_req, res) => {
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(datadog({
-    "response_code":true,
-    "tags": ["app:my_app"]
-}))
 app.use("/", router);
 
 export const sample = functions.https.onRequest(app);
