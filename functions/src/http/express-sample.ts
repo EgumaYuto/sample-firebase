7d11f532069aee01e9c74aeabc0eb1b2ea8652ea
yuto.eguma <yuto.eguma@gmail.com>
2021-12-31 17:50:39 +0900

    loggerを使ってみる

--- functions/src/http/express-sample.ts
+++ functions/src/http/express-sample.ts
@@ -1,15 +1,21 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
+import { MyLogger } from "../util/MyLogger";
+
+const logger = MyLogger.create("express-sample")
 
 const router = express.Router();
 router.get("/", (_req, res) => {
+  logger.info("GET /")
   res.json({path: "/"});
 });
 router.get("/path1", (_req, res) => {
+  logger.info("GET /path1")
   res.json({path: "/path1"});
 });
 router.post("/message", async (req, res) => {
+  logger.info("POST /message")
   const message = req.body.message
   const writeResult = await admin.firestore()
       .collection("message").add({original: message});
