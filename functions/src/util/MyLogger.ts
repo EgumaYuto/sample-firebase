9e0c089d89ead46e3254cd51aacbbfa73c074771
yuto.eguma <yuto.eguma@gmail.com>
2021-12-31 17:47:48 +0900

    Loggerの準備

---
+++ functions/src/util/MyLogger.ts
@@ -0,0 +1,73 @@
+import { Logger, getLogger, configure, addLayout } from "log4js";
+
+export class MyLogger {
+  private logger: Logger;
+  private constructor(fileName: string) {
+    addLayout('json', (config) => (logEvent) => {
+      return JSON.stringify({
+        fileName: fileName,
+        severity: logEvent.level.levelStr,
+        message: this.formatMessage(logEvent.data),
+        timestamp: logEvent.startTime
+      }) + config.separator
+    })
+    configure({
+      appenders: {
+        out: {
+          type: 'stdout', layout: {type : 'json', separator: ''}
+        }
+      },
+      categories: {
+        default: { 
+          appenders: ['out'], level: 'all'
+        }
+      }
+    })
+    this.logger = getLogger();
+  }
+
+  /**
+   * TODO どんなログを出すことが多いのか次第で出し分ける
+   * @param data 
+   * @returns 
+   */
+  private formatMessage(data: any[]) : string | undefined{
+    if (!data || data.length === 0) {
+      return undefined
+    }
+    let message = ''
+    const separator = ', '
+    for (let index = 0; index < data.length; index++) {
+      if (data[index] instanceof Error) {
+        const d = data[index]
+        message += d.stack
+      } else {
+        message += data[index]
+      }
+      message += separator
+    }
+    return message.substring(0, message.length - separator.length)
+  }
+
+  public static create = (fileName: string) => new MyLogger(fileName);
+
+  public debug = (message: string, ...args: any[]): void => {
+    this.logger.debug(message, ...args);
+  };
+
+  public info = (message: string, ...args: any[]): void => {
+    this.logger.info(message, ...args);
+  };
+
+  public warning = (message: string, ...args: any[]): void => {
+    this.logger.warn(message, ...args);
+  };
+
+  public error = (message: string, ...args: any[]): void => {
+    this.logger.error(message, ...args);
+  };
+
+  public critical = (message: string, ...args: any[]): void => {
+    this.logger.fatal(message, ...args);
+  };
+}
