c22e3a5a3102ec9069a97edfb0ec4011a5b6b801
yuto.eguma <yuto.eguma@gmail.com>
2022-01-03 16:01:34 +0900

    Loggerの更新

--- functions/src/util/MyLogger.ts
+++ functions/src/util/MyLogger.ts
@@ -1,16 +1,18 @@
-import { Logger, getLogger, configure, addLayout } from "log4js";
+import { Logger, getLogger, configure, addLayout, LoggingEvent } from "log4js";
+
+interface LogPayload {
+  fileName: string;
+  severity: string;
+  message: string;
+  timestamp: Date;
+}
 
 export class MyLogger {
   private logger: Logger;
   private constructor(fileName: string) {
     addLayout("json", (config) => (logEvent) => {
       return (
-        JSON.stringify({
-          fileName: fileName,
-          severity: logEvent.level.levelStr,
-          message: this.formatMessage(logEvent.data),
-          timestamp: logEvent.startTime,
-        }) + config.separator
+        JSON.stringify(this.buildPayload(fileName, logEvent)) + config.separator
       );
     });
     configure({
@@ -31,22 +33,42 @@ export class MyLogger {
   }
 
   /**
-   * TODO どんなログを出すことが多いのか次第で出し分ける
-   * @param data
-   * @returns
+   * ログ出力するペイロードを組み立てる
+   *
+   * @param fileName ファイル名
+   * @param logEvent ログイベント
+   * @return ログ出力するペイロード
    */
-  private formatMessage(data: any[]): string | undefined {
+  private buildPayload(fileName: string, logEvent: LoggingEvent): LogPayload {
+    return {
+      fileName: fileName,
+      severity: logEvent.level.levelStr,
+      message: this.formatMessage(logEvent.data),
+      timestamp: logEvent.startTime,
+    };
+  }
+
+  /**
+   * 渡されたメッセージのタイプごとに、出力を調整する
+   *
+   * @param data
+   * @return formated message
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private formatMessage(data: any[]): string {
     if (!data || data.length === 0) {
-      return undefined;
+      return "";
     }
     let message = "";
     const separator = ", ";
     for (let index = 0; index < data.length; index++) {
-      if (data[index] instanceof Error) {
-        const d = data[index];
+      const d = data[index];
+      if (d instanceof Error) {
         message += d.stack;
+      } else if (typeof d === "string") {
+        message += d;
       } else {
-        message += data[index];
+        message += JSON.stringify(d);
       }
       message += separator;
     }
@@ -55,22 +77,27 @@ export class MyLogger {
 
   public static create = (fileName: string) => new MyLogger(fileName);
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public debug = (message: string, ...args: any[]): void => {
     this.logger.debug(message, ...args);
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public info = (message: string, ...args: any[]): void => {
     this.logger.info(message, ...args);
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public warning = (message: string, ...args: any[]): void => {
     this.logger.warn(message, ...args);
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public error = (message: string, ...args: any[]): void => {
     this.logger.error(message, ...args);
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public critical = (message: string, ...args: any[]): void => {
     this.logger.fatal(message, ...args);
   };
